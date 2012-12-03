caceb929cd14b8cff9b0910f4b873b3eb4248f43
Jonas Trevisan <jonhkr@gmail.com>
2012-12-03 16:27:12 -0200

    better example

--- example.py
+++ example.py
@@ -1,21 +1,51 @@
 from mongomodels import Document, ValidationError, Field as field
+from bson.objectid import ObjectId
+
+import re
+
+
+def email_validator(email):
+    reg = re.compile(r'\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b', re.IGNORECASE)
+    return True if reg.match(email) else (False, 'Invalid email address')
+
+
+class User(Document):
+
+    id = field(type=ObjectId)
+    firstname = field(type=basestring, required=True)
+    lastname = field(type=basestring, required=True)
+    email = field(type=basestring, validators=[email_validator])
+    login = field(type=basestring, required=True)
+    passwd = field(type=basestring, required=True)
+
 
 class Transaction(Document):
 
-    description = field(type=str, required=True)
-    category = field(type=str, required=True)
+    id = field(type=ObjectId)
+    uid = field(type=ObjectId, required=True)
+    description = field(type=basestring, required=True)
+    category = field(type=basestring, required=True)
 
+
+u = User()
+u.id = ObjectId()
+u.firstname = 'Joao'
+u.lastname = 'Pereira'
+u.email = 'joao@example.com'
+u.login = 'jp'
+u.passwd = 'passwd'
 
 a = Transaction()
-
-a.from_mongo({
-    'description': 'teste',
-    'category': 'cat'
-})
+a.uid = u.id
+a.description = 'Teste'
+a.category = 'Teste'
 
 try:
-    a.validate()
+    print u.to_mongo()
 except ValidationError as e:
     print e.errors
 
-print a.to_json()
+try:
+    print a.to_mongo()
+except ValidationError as e:
+    print e.errors
--- mongomodels/__init__.py
+++ mongomodels/__init__.py
@@ -1,5 +1,6 @@
 from bson import json_util
 
+
 class Field:
     def __init__(self, *args, **kwargs):
         self.meta = dict(*args, **kwargs)
@@ -21,7 +22,7 @@ class Document(object):
         self.fields = {}
         self.required_fields = []
         self.field_validators = {}
-        
+
         self.prepare_fields()
 
     def prepare_fields(self):
@@ -35,8 +36,8 @@ class Document(object):
                     self.field_validators[attr] = attrv.meta['validators']
                 setattr(self, attr, None)
 
-    def is_empty(self, field):
-        if field is None or field is '':
+    def is_empty(self, fv):
+        if fv is None or fv is '':
             return True
         return False
 
@@ -45,30 +46,31 @@ class Document(object):
             self.errors[field] = []
         self.errors[field].append(error)
 
-    def validate_required(self, f):
+    def validate_required(self, f, fv):
         if f in self.required_fields:
-            if self.is_empty(getattr(self, f)):
+            if self.is_empty(fv):
                 self.append_error(f, self.FIELD_REQUIRED_MESSAGE)
 
-    def validate_with_validators(self, f):
+    def validate_with_validators(self, f, fv):
         if f in self.field_validators.keys():
             validators = self.field_validators[f]
             for validator in validators:
-                valid = validator(field)
+                valid = validator(fv)
                 if not valid is True:
                     self.append_error(f, valid[1])
 
-    def validate_type(self, f, v):
-        fv = getattr(self, f)
+    def validate_type(self, v, f, fv):
         if not isinstance(fv, v.meta['type']) and fv:
             self.append_error(f, self.INVALID_FIELD_TYPE_MESSAGE
                               % v.meta['type'].__name__)
 
     def validate(self):
+        self.errors = {}
         for f, v in self.fields.items():
-            self.validate_required(f)
-            self.validate_with_validators(f)
-            self.validate_type(f, v)
+            fv = getattr(self, f)
+            self.validate_required(f, fv)
+            self.validate_with_validators(f, fv)
+            self.validate_type(v, f, fv)
 
         if self.errors:
             raise ValidationError(self.errors)
