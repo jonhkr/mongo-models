f8eaabe23ac548527bcfc761e1c57d57b791e39d
Jonas Trevisan <jonhkr@gmail.com>
2012-12-14 16:25:37 -0200

    modif

--- example.py
+++ example.py
@@ -1,51 +1,25 @@
-from mongomodels import Document, ValidationError, Field as field
+from mongomodels import Document, Field as field, Embed as embed
 from bson.objectid import ObjectId
-
-import re
-
-
-def email_validator(email):
-    reg = re.compile(r'\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b', re.IGNORECASE)
-    return True if reg.match(email) else (False, 'Invalid email address')
-
 
 class User(Document):
 
-    __collection__ = 'user'
-
-    firstname = field(required=True)
-    lastname = field(required=True)
-    email = field(validators=[email_validator])
-    login = field(required=True)
-    passwd = field(required=True)
+    firstname = field(type=basestring)
+    lastname = field(type=basestring)
+    email = field(type=basestring)
+    login = field(type=basestring)
+    passwd = field(type=basestring)
 
 
 class Transaction(Document):
 
-    uid = field(required=True)
-    description = field(required=True)
-    category = field(required=True)
+    uid = field(type=ObjectId)
+    description = field(type=basestring)
+    category = field(type=basestring)
+    embed = embed(type=User)
+    
 
+a = User()
 
-u = User()
-u.id = ObjectId()
-u.firstname = 'Joao'
-u.lastname = 'Pereira'
-u.email = 'joao@example.com'
-u.login = 'jp'
-u.passwd = 't'
+b = Transaction()
 
-a = Transaction()
-a.uid = u.id
-a.description = 'Teste'
-a.category = 'Teste'
-
-try:
-    print u.to_mongo()
-except ValidationError as e:
-    print e.errors
-
-try:
-    print a.to_mongo()
-except ValidationError as e:
-    print e.errors
+print b.to_mongo()
--- mongomodels/__init__.py
+++ mongomodels/__init__.py
@@ -1,84 +1,51 @@
 from bson import json_util
 from bson.objectid import ObjectId
 
-class Field:
+
+class Field(object):
     def __init__(self, *args, **kwargs):
-        self.meta = dict(*args, **kwargs)
+        self.__dict__.update(dict(*args, **kwargs))
 
 
-class ValidationError(Exception):
-    def __init__(self, errors):
-        Exception.__init__(self, errors)
-
-        self.errors = errors
+class Embed(Field):
+    pass
 
 
 class Document(object):
 
     def __init__(self):
-        self.errors = {}
         self.fields = {}
-        self.required_fields = []
-        self.field_validators = {}
-
+        self.embed_fields = []
         self.prepare_fields()
 
     def prepare_fields(self):
         for attr in dir(self):
             attrv = getattr(self, attr)
             if isinstance(attrv, Field):
+                v = None
+                if isinstance(attrv, Embed):
+                    v = attrv.type()
+                    self.embed_fields.append(attr)
+                setattr(self, attr, v)
                 self.fields[attr] = attrv
-                if 'required' in attrv.meta and attrv.meta['required']:
-                    self.required_fields.append(attr)
-                if 'validators' in attrv.meta:
-                    self.field_validators[attr] = attrv.meta['validators']
-                setattr(self, attr, None)
-
-    def is_empty(self, fv):
-        if fv is None or fv is '':
-            return True
-        return False
-
-    def append_error(self, field, error):
-        if field not in self.errors:
-            self.errors[field] = []
-        self.errors[field].append(error)
-
-    def validate_required(self, f, fv):
-        if f in self.required_fields:
-            if self.is_empty(fv):
-                self.append_error(f, 'Field required')
-
-    def validate_with_validators(self, f, fv):
-        if f in self.field_validators.keys():
-            validators = self.field_validators[f]
-            for validator in validators:
-                valid = validator(fv)
-                if not valid is True:
-                    self.append_error(f, valid[1])
-
-    def validate(self):
-        self.errors = {}
-        for f, v in self.fields.items():
-            fv = getattr(self, f)
-            self.validate_required(f, fv)
-            self.validate_with_validators(f, fv)
-
-        if self.errors:
-            raise ValidationError(self.errors)
 
     def to_mongo(self):
-        self.validate()
         mongodoc = {}
         for field in self.fields.keys():
             mongodoc[field] = getattr(self, field)
+            if field in self.embed_fields:
+                mongodoc[field] = mongodoc[field].to_mongo()
         return mongodoc
 
     def from_mongo(self, data):
         if not '_id' in self.fields.keys() and '_id' in data.keys():
             self.fields['_id'] = Field()
         for f in self.fields.keys():
-            setattr(self, f, data[f])
+            obj = data[f]
+            if f in self.embed_fields:
+                obj = self.fields[f].cls()
+                obj.from_mongo(data[f])
+            setattr(self, f, obj)
 
     def to_json(self):
         return json_util.dumps(self.to_mongo())
@@ -86,4 +53,9 @@ class Document(object):
     def from_json(self, data):
         self.from_mongo(json_util.loads(data))
 
-__all__ = ['Document', 'Field', 'ValidationError']
+    def __setattr__(self, name, value):
+        if hasattr(self, 'fields') and name in self.fields.keys():
+            if not isinstance(value, self.fields[name].type):
+                raise ValueError('Attribute %s expecting instance of %s, received %s' %
+                                 (name, self.fields[name].type.__name__, type(value).__name__))
+        super(Document, self).__setattr__(name, value)
